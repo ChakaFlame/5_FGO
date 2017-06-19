0708667bbb10d6cf5d5c7d4e1b8af53897b225fc
komaki-yasuhiro <komaki.yasuhiro@isid-intertech.co.jp>
2017-06-19 15:56:09 +0900

    いけた？

---
+++ src/tsys/sales/logic/Order.java
@@ -0,0 +1,96 @@
+import java.util.Date;
+
+public class Order {
+	/** 注文番号 */
+	private int orderNo;
+	/** 注文日時 */
+	private Date orderDate;
+	/** 注文合計金額 */
+	private int orderTotal;
+	/** 顧客コード */
+	private String memberCode;
+	/** 決済方法 */
+	private String payment;
+	/**
+	 *
+	 */
+	public Order() {
+	}
+	/**
+	 * @param orderNo
+	 * @param orderDate
+	 * @param orderTotal
+	 * @param memberCode
+	 * @param payment
+	 */
+	public Order(int orderNo, Date orderDate, int orderTotal, String memberCode, String payment) {
+		super();
+		this.orderNo = orderNo;
+		this.orderDate = orderDate;
+		this.orderTotal = orderTotal;
+		this.memberCode = memberCode;
+		this.payment = payment;
+	}
+	/**
+	 * @return orderNo
+	 */
+	public int getOrderNo() {
+		return orderNo;
+	}
+	/**
+	 * @return orderDate
+	 */
+	public Date getOrderDate() {
+		return orderDate;
+	}
+	/**
+	 * @return orderTotal
+	 */
+	public int getOrderTotal() {
+		return orderTotal;
+	}
+	/**
+	 * @return memberCode
+	 */
+	public String getMemberCode() {
+		return memberCode;
+	}
+	/**
+	 * @return payment
+	 */
+	public String getPayment() {
+		return payment;
+	}
+	/**
+	 * @param orderNo セットする orderNo
+	 */
+	public void setOrderNo(int orderNo) {
+		this.orderNo = orderNo;
+	}
+	/**
+	 * @param orderDate セットする orderDate
+	 */
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+	/**
+	 * @param orderTotal セットする orderTotal
+	 */
+	public void setOrderTotal(int orderTotal) {
+		this.orderTotal = orderTotal;
+	}
+	/**
+	 * @param memberCode セットする memberCode
+	 */
+	public void setMemberCode(String memberCode) {
+		this.memberCode = memberCode;
+	}
+	/**
+	 * @param payment セットする payment
+	 */
+	public void setPayment(String payment) {
+		this.payment = payment;
+	}
+
+
+}
---
+++ src/tsys/sales/logic/OrderDetail.java
@@ -0,0 +1,94 @@
+
+public class OrderDetail {
+	/** 注文番号 */
+	private int orderNo;
+	/** 商品コード */
+	private String itemCode;
+	/** 商品名 */
+	private String name;
+	/** 原価 */
+	private int price;
+	/** 予約数 */
+	private int quantity;
+	/**
+	 *
+	 */
+	public OrderDetail() {
+	}
+	/**
+	 * @param orderNo
+	 * @param itemCode
+	 * @param name
+	 * @param price
+	 * @param quantity
+	 */
+	public OrderDetail(int orderNo, String itemCode, String name, int price, int quantity) {
+		this.orderNo = orderNo;
+		this.itemCode = itemCode;
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	/**
+	 * @return orderNo
+	 */
+	public int getOrderNo() {
+		return orderNo;
+	}
+	/**
+	 * @param orderNo セットする orderNo
+	 */
+	public void setOrderNo(int orderNo) {
+		this.orderNo = orderNo;
+	}
+	/**
+	 * @return itemCode
+	 */
+	public String getItemCode() {
+		return itemCode;
+	}
+	/**
+	 * @param itemCode セットする itemCode
+	 */
+	public void setItemCode(String itemCode) {
+		this.itemCode = itemCode;
+	}
+	/**
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name セットする name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return price
+	 */
+	public int getPrice() {
+		return price;
+	}
+	/**
+	 * @param price セットする price
+	 */
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	/**
+	 * @return quantity
+	 */
+	public int getQuantity() {
+		return quantity;
+	}
+	/**
+	 * @param quantity セットする quantity
+	 */
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+
+}
