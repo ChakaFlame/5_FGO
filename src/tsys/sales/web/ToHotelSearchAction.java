c1b2df2ab7cd31aaf0fd6d0790e57cdbc6f77a0b
koshi-takaaki <koshi.takaaki@isid.co.jp>
2017-06-20 16:25:29 +0900

    ｄｄ

--- src/tsys/sales/web/ToHotelSearchAction.java
+++ src/tsys/sales/web/ToHotelSearchAction.java
@@ -9,24 +9,8 @@ import tsys.sales.logic.HotelDetailLogic;
 
 public class ToHotelSearchAction {
 	public String execute(HttpServletRequest req){
-		String page = "/HotelSearch.jsp";							//入力画面を戻り値に設定
+		String page = "/HotelSearch/HotelSearch.jsp";							//入力画面を戻り値に設定
 
-		String hotelCode = req.getParameter("hotelCode");
-
-		try{
-			HotelDetailLogic hotelDetailLogic = new HotelDetailLogic();
-			Hotel hotel = hotelDetailLogic.getDetail(hotelCode);
-
-			req.setAttribute("Hotel", hotel);
-			page = "/HotelSearch.jsp";								//ホテル商品
-		} catch (SalesBusinessException e){
-			e.printStackTrace();
-			req.setAttribute("error", e.getMessage());
-		} catch (SalesSystemException e){
-			e.printStackTrace();
-			req.setAttribute("error", e.getMessage());
-			page = "/SalesErrorView.jsp";
-		}
 			return page;
 	}
 }
