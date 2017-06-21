642069e590c0deb74012fb56e81c866de89aa81d
koshi-takaaki <koshi.takaaki@isid.co.jp>
2017-06-21 15:35:22 +0900

    session.invalidate();をToLogoutAction内に記述

--- src/tsys/sales/web/ToLogoutAction.java
+++ src/tsys/sales/web/ToLogoutAction.java
@@ -1,6 +1,7 @@
 package tsys.sales.web;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import tsys.sales.common.SalesBusinessException;
 import tsys.sales.common.SalesSystemException;
@@ -8,6 +9,10 @@ import tsys.sales.entity.Hotel;
 
 public class ToLogoutAction {
 	public String execute(HttpServletRequest req){
+
+		HttpSession session = req.getSession(true);				//セッションの破棄
+		session.invalidate();
+
 		String page = "/Login/Logout.jsp";							//入力画面を戻り値に設定
 
 			return page;
