dbe1ad225266f0119c4b7fc35d7013103c1cb322
komaki-yasuhiro <komaki.yasuhiro@isid-intertech.co.jp>
2017-06-23 16:46:18 +0900

    実装完

--- src/tsys/sales/web/OrderDetailAction.java
+++ src/tsys/sales/web/OrderDetailAction.java
@@ -1,49 +1,30 @@
 package tsys.sales.web;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import tsys.sales.entity.Hotel;
 import tsys.sales.entity.Order;
 import tsys.sales.entity.OrderDetail;
-import tsys.sales.logic.HotelDetailLogic;
-import tsys.sales.logic.HotelSearchLogic;
 import tsys.sales.logic.OrderDetailLogic;
+import tsys.sales.logic.OrderListLogic;
 
 public class OrderDetailAction {
-	public String execute(HttpServletRequest req) {
-
+	public String execute(HttpServletRequest req) throws SQLException {
+		String page=null;
 		HttpSession session = req.getSession();
+		OrderListLogic orderListLogic=new OrderListLogic();
 
-		String page = null;
-		String orderindex = (String) req.getParameter("ORDER_INDEX");
-		Order order = null;
-		ArrayList<Order> orderList = (ArrayList<Order>) session.getAttribute("orderList");
-
-		session.setAttribute("order", req.getParameter("index"));
-		session.removeAttribute("orderList");
-
-		try {
-			order = (Order) session.getAttribute(orderindex);
-
-			ArrayList<OrderDetail> orderDetailList= OrderDetailLogic.orderDetail(order.getOrderNo());
-			ArrayList<Hotel> hotelList=HotelSearchLogic.searchHotel(orderDetailList.get(2));
+		session.setAttribute("orderNo", req.getParameter("orderNo"));
+		ArrayList<OrderDetail> orderDetailList = OrderDetailLogic.orderDetail((int) session.getAttribute("orderNo"));
+		session.setAttribute("orderDetail", orderDetailList);
 
 
-			for (OrderDetail orderDetail2 : orderDetailList) {
-				session.setAttribute("orderDetail", orderDetail2);
-			}
-
-			if (orderList == null) {
-				req.setAttribute("message", "注文履歴なし。");
-				return page;
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			req.setAttribute("error", e.getMessage());
-		}
+		int orderNoInt = (int) session.getAttribute("orderNo");
+		ArrayList<Order> orderList = orderListLogic.orderList(orderNoInt);
+		session.setAttribute("order", orderList);
 		return page;
 	}
 }
