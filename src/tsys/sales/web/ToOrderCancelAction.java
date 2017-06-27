7148f6edc846f8421a3a882f9d3c323d0c615c92
Toriumi-Hiomi <toriumi.hiromi@isid-intertech.co.jp>
2017-06-27 16:11:37 +0900

    なおした

--- src/tsys/sales/web/ToOrderCancelAction.java
+++ src/tsys/sales/web/ToOrderCancelAction.java
@@ -2,6 +2,7 @@ package tsys.sales.web;
 
 import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 
@@ -16,10 +17,28 @@ public class ToOrderCancelAction {
 		String page = "/Order/OrderConfirmation.jsp"; // 入力画面を戻り値に設定
 
 		Calendar oneWeek = Calendar.getInstance(); // 今日の日付を取得
-		oneWeek.add(Calendar.DAY_OF_MONTH, 7); // 今日から７日後に設定
+		/*
+		 * デバッグ用コード
+		 */
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		System.out.println(oneWeek);
+		/*
+		 * デバッグ用コード終了
+		 */
+		oneWeek.add(Calendar.MONTH, 1);
+		oneWeek.add(Calendar.DATE, 8); // 今日から７日後に設定
 		boolean flag = true; // 取消可能かどうかの判断
 		String hotelDateStr; // StringのhotelDate
 		Calendar hotelDateCal = Calendar.getInstance(); // CalendarのhotelDate
+
+
+		/*
+		 * デバッグ用コード
+		 */
+		System.out.println(oneWeek);
+		/*
+		 * デバッグ用コード終了
+		 */
 
 		HttpSession session = req.getSession();
 		ArrayList<Hotel> hotelList = (ArrayList<Hotel>) session.getAttribute("hotelList");
@@ -32,7 +51,7 @@ public class ToOrderCancelAction {
 				// TODO 自動生成された catch ブロック
 				e.printStackTrace();
 			}
-			if(oneWeek.after(hotelDateCal)){
+			if(oneWeek.before(hotelDateCal)){
 				flag = false;
 			}
 		}
